31391ddd7aa2a1d1d501b81deb376d0bb3616826
Serge Bogoduhov <zz9250347500@gmail.com>
2023-05-27 21:20:27 +0300

    got

--- Home_1/Hom-1.py
+++ Home_1/Hom-1.py
@@ -9,14 +9,12 @@ N = int(input("Введите количество монет: "))
 orel = 0
 reshka = 0
 for i in range(N):
-    x = int(input("Орел(1) или решка(0)? "))
+    x = int(input())
     if x == 1:
         orel += 1
     else:
         reshka += 1
 if orel < reshka:
     print("Переверните {orel} монет с орла на решку")
-elif orel == reshka:
-    print("Количество орлов и решек одинаково, по {orel} штук")
 else:
     print("Переверните {reshka} монет с решки на орла")
